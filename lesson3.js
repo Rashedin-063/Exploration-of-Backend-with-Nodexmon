5a17e076e282aa909827e50f97922b48a3de3cbd
Rashedin-063 <rashedinislam.06@gmail.com>
2025-01-28 22:24:38 +0600

    lesson 3 is done

---
+++ lesson3.js
@@ -0,0 +1,18 @@
+// os, path
+
+// const os = require('os');
+// console.log(os)
+
+// console.log.userInfo())userInfo())
+// console.log(os.homedir())
+// console.log(os.hostname())
+// console.log(os.totalmem());
+// console.log(os.freemem())
+
+const { totalmem, freemem } = require('os')
+
+console.log(totalmem(), freemem()) // total memory
+
+
+
+
