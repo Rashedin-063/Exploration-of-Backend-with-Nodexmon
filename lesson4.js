d51f1ccbbcc8c3500cb083609566eed0e5168220
Rashedin-063 <rashedinislam.06@gmail.com>
2025-01-28 22:34:16 +0600

    lesson 4 done

---
+++ lesson4.js
@@ -0,0 +1,21 @@
+// console.log(__dirname)
+
+// console.log(__filename)
+
+const path = require('path');
+
+// console.log(path)
+
+// const extensionName = path.extname("lesson4.js")
+
+// console.log(extensionName)
+
+// const join = path.join(__dirname, "/../hello.html");
+
+// console.log(join)
+
+
+
+
+
+
