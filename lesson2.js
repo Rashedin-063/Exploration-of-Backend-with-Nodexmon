2a1589049cc596251a83ff4e81597d88711f6699
Rashedin-063 <rashedinislam.06@gmail.com>
2025-01-28 22:17:45 +0600

    lesson 2 is done

---
+++ lesson2.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+// console.log(fs)
+
+// fs.writeFile('demo1.txt', "My name is Rashedin Islam.", (err) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log("Successful")
+//   }
+// });
+
+// fs.appendFile('demo1.txt', " I am a working at a Swedish Company named Coursfy.", (err) => {
+//     if (err) {
+//     console.log(err)
+//   } else {
+//     console.log("Successful")
+//   }
+// })
+// fs.readFile('demo1.txt', 'utf-8', (err, data) => {
+//     if (err) {
+//     console.log(err)
+//   } else {
+//       console.log("Successful")
+//       console.log(data)
+      
+//   }
+// // })
+// fs.rename('demo1.txt', 'demo2.txt', (err) => {
+//     if (err) {
+//     console.log(err)
+//   } else {
+//       console.log("Successful")
+//   }
+// })
+// })
+
+// fs.unlink('demo2.txt', (err) => {
+//     if (err) {
+//     console.log(err)
+//   } else {
+//       console.log("Successful")
+//   }
+// })
+
+// fs.exists('demo2.txt', (result) => {
+//     if (result) {
+//     console.log('found')
+//   } else {
+//       console.log("Not found")
+//   }
+// })
+
+
+
+// fs.writeFileSync('demo.txt', 'This is a demo')
+
+// fs.existsSync('demo.txt')
+
