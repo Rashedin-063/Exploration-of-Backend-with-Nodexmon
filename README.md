cefa1e53a42a1afb5695d7649556f9510f23b7cd
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-24 22:35:59 +0600

    updated

---
+++ README.md
@@ -0,0 +1,4 @@
+## This is the public repo of my exploration of Backend with Node JS, Express JS and MongoDb
+
+### Tutorial I have followed
+- [Anisul Islam Node.js Tutorial]()
--- lessons/lesson5.js
+++ lessons/lesson5.js
@@ -4,6 +4,10 @@ const port = 5000;
 const hostname = '127.0.0.1'
 
 const myServer = http.createServer((req, res) => {
+   res.writeHead(202, { 'Content-Type': 'text/html' });
+   res.write('<h1>hello</h1>');
+   res.write('<h1>hello</h1>');
+   res.write('<h1>hello</h1>');
   res.end("hello from you first server")
 })
 
