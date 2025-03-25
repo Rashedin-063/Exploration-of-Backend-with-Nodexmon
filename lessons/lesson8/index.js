86601512cdb034529711c83de2d12423cc4e41cf
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-25 10:00:06 +0600

    routing done with raw node.js

--- lessons/lesson8/index.js
+++ lessons/lesson8/index.js
@@ -7,12 +7,34 @@ const server = http.createServer((req, res) => {
   console.log(req.url)
 
   if (req.url === '/') {
-    fs.readFile('index.html', 'utf8', (err, data) => {
-res.
+    fs.readFile('index.html', (err, data) => {
+      if (err) console.log('error reading data')
+      
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end();
     })
   }
-  
-  res.end('welcome to the server')
+  // about route
+  if (req.url === '/about') {
+    fs.readFile('about.html', (err, data) => {
+      if (err) console.log('error reading data')
+      
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end();
+    })
+  }
+  // contact route
+  if (req.url === '/contact') {
+    fs.readFile('contact.html', (err, data) => {
+      if (err) console.log('error reading data')
+      
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end();
+    })
+  }
 })
 
 server.listen(PORT, hostName, () => {
