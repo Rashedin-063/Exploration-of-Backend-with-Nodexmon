bbac9dbfc481c97ec72957067d363bc8b888ad5d
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-26 14:21:44 +0600

    request query parameter explored

--- 02 - Express/Lesson-02/index.js
+++ 02 - Express/Lesson-02/index.js
@@ -3,7 +3,14 @@ const app = express();
 const PORT = 3000
 
 
+app.get('/', (req, res) => {
+  const {id, name} = req.query
+  // const name = req.query.name;
+  res.send(`<h1>Student name is ${name} and his id is ${id}</h1>`);
+})
+
+
 app.listen(PORT, () => {
-  console.log(`Server is running at ${PORT}`)
+  console.log(`Server is running at https://localhost:${PORT}`)
   
 })
