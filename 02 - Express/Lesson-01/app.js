5e7034c2d064d3ca3da336acda5d480d3c137dc4
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-26 13:13:05 +0600

    http response exploration done

--- 02 - Express/Lesson-01/app.js
+++ 02 - Express/Lesson-01/app.js
@@ -4,26 +4,49 @@ const app = express();
 const userRouter = require('./routes/users.route')
 
 
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to my server!!!</h1>');
-  res.end();
-});
 
 app.use('/api/user', userRouter);
+
+
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Welcome to my server!!!</h1>');
+  // res.end();
+
+  res.statusCode = 200;
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 app.use('/register', (req, res) => {
   // res.status(200).json({
   //   "success": true,
   //   "message": "Registration successful"
   // })
-  res.redirect('/login')
+  // res.redirect('/login')
+
+  res.statusCode = 200;
+  res.sendFile(__dirname + '/views/register.html');
+
 })
 app.use('/login', (req, res) => {
-  res.send('<h1>Login page</h1>');
+  // res.send('<h1>Login page</h1>');
+
+  // res.cookie("name", "sraboni")
+  // res.cookie("age", "30")
+  res.clearCookie("name");
+  res.clearCookie("age");
+
+  res.append("id", "1300000")
+
+  //  res.statusCode = 200;
+  // res.sendFile(__dirname + '/views/login.html');
+  res.end()
 })
 
 app.use((req, res) => {
   res.send('<h1>Ooops! Page Not Found</h1>')
+
+  
 })
 
 
