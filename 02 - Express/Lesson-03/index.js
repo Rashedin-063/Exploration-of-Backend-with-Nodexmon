2225715af571a44aee04b29ad62ef2dc3c50e8ed
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-26 19:10:48 +0600

    environment variable used

--- 02 - Express/Lesson-03/index.js
+++ 02 - Express/Lesson-03/index.js
@@ -1,22 +1,26 @@
-console.log('hello')
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
-app.get('/products/:id([0-9]{3})', (req, res) => { 
-  res.send(`<h2> ID = ${req.params.id} </h2>`);
-})
+const PORT = process.env.PORT || 3001;
 
-app.get('/products/:title([a-zA-Z-_]+)', (req, res) => { 
-  res.send(`<h2> Title = ${req.params.title} </h2>`);
-})
+// regular expressions
 
+// app.get('/products/:id([0-9]{3})', (req, res) => {
+//   res.send(`<h2> ID = ${req.params.id} </h2>`);
+// })
+// app.get('/products/:title([a-zA-Z-_]+)', (req, res) => {
+//   res.send(`<h2> Title = ${req.params.title} </h2>`);
+// })
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to my Express Server</h1>');
+});
 
 app.get('*', (req, res) => {
   res.send('<h1>404 Not Found</h1>');
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`)
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
