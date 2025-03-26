47f5ab4db862a074fb58434baefa6b3e0b4c0508
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-26 11:05:40 +0600

    crud tested with postman

--- 02 - Express/Lesson-01/app.js
+++ 02 - Express/Lesson-01/app.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Welcome to my server!!!');
 });
+
+app.post('/', (req, res) => {
+  res.send('Posting some data....')
+})
+
+app.put('/', (req, res) => {
+  res.send('Updating some data....')
+})
+
+app.delete('/', (req, res) => {
+  res.send('Deleting some data....')
+})
 
 
 module.exports = {
