ea4434631c1fdbabc2bc1ca91c07b2fe15097b80
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-26 17:08:22 +0600

    area calculator project done

--- 02 - Express/Project-01/index.js
+++ 02 - Express/Project-01/index.js
@@ -8,7 +8,26 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => { 
-  res.send('Hello from my server!');
+  res.sendFile(__dirname + '/views/index.html');
+})
+app.get('/circle', (req, res) => { 
+  res.sendFile(__dirname + '/views/circle.html');
+})
+app.get('/triangle', (req, res) => { 
+  res.sendFile(__dirname + '/views/triangle.html');
+})
+
+app.post('/circle', (req, res) => {
+  const { radius } = req.body;
+    const area = ( Math.PI * radius * radius)
+    res.send(`<h1> Area of Circle is : ${area} </h1>`);
+  
+})
+
+app.post('/triangle', (req, res) => {
+  const { base, height } = req.body
+  const area = (0.5 * height * base);
+  res.send(`<h1> Area of Triangle is : ${area} </h1>`);
 })
 
 
