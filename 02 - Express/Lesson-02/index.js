f8fadf90278407b2d8bef69ae6ae3ad6a0fd425d
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-26 14:38:33 +0600

    req.headers explored

--- 02 - Express/Lesson-02/index.js
+++ 02 - Express/Lesson-02/index.js
@@ -2,17 +2,30 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// query
 // app.get('/', (req, res) => {
 //   const {id, name} = req.query
 //   // const name = req.query.name;
 //   res.send(`<h1>Student name is ${name} and his id is ${id}</h1>`);
 // })
 
-app.get('/userId/:id/age/:age', (req, res) => {
-  const { id, age } = req.params;
+// params
+// app.get('/userId/:id/age/:age', (req, res) => {
+//   const { id, age } = req.params;
 
-  res.send(`<h1>Student id  ${id} is ${age} years old</h1>`);
-});
+//   res.send(`<h1>Student id  ${id} is ${age} years old</h1>`);
+// });
+
+// headers
+app.get('/', (req, res) => {
+  const { id, name } = req.headers
+
+  console.log(id, name, req.headers)
+  
+  
+  
+  res.send(`<h1>Student name is ${name} and his id is ${id}</h1>`);
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at https://localhost:${PORT}`);
