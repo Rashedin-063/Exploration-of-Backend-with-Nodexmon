6978c183e097864f1c6418c4437a4abd94c431c0
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-26 12:50:49 +0600

    http response explored

--- 02 - Express/Lesson-01/app.js
+++ 02 - Express/Lesson-01/app.js
@@ -9,7 +9,18 @@ app.get('/', (req, res) => {
   res.end();
 });
 
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
+
+app.use('/register', (req, res) => {
+  // res.status(200).json({
+  //   "success": true,
+  //   "message": "Registration successful"
+  // })
+  res.redirect('/login')
+})
+app.use('/login', (req, res) => {
+  res.send('<h1>Login page</h1>');
+})
 
 app.use((req, res) => {
   res.send('<h1>Ooops! Page Not Found</h1>')
