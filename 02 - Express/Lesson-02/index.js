b5744244f7785a911a749513566feca21050f4f3
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-26 14:31:00 +0600

    route parameter explored

--- 02 - Express/Lesson-02/index.js
+++ 02 - Express/Lesson-02/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = 3000
+const PORT = 3000;
 
+// app.get('/', (req, res) => {
+//   const {id, name} = req.query
+//   // const name = req.query.name;
+//   res.send(`<h1>Student name is ${name} and his id is ${id}</h1>`);
+// })
 
-app.get('/', (req, res) => {
-  const {id, name} = req.query
-  // const name = req.query.name;
-  res.send(`<h1>Student name is ${name} and his id is ${id}</h1>`);
-})
+app.get('/userId/:id/age/:age', (req, res) => {
+  const { id, age } = req.params;
 
+  res.send(`<h1>Student id  ${id} is ${age} years old</h1>`);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running at https://localhost:${PORT}`)
-  
-})
+  console.log(`Server is running at https://localhost:${PORT}`);
+});
