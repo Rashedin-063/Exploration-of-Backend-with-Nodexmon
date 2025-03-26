677de913e1fb3e028cc424ab6030933a1329b65b
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-26 10:19:42 +0600

    updated

--- 02 - Express/Lesson-01/index.js
+++ 02 - Express/Lesson-01/index.js
@@ -2,9 +2,7 @@ const { app } = require('./app');
 const PORT = 3000;
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
