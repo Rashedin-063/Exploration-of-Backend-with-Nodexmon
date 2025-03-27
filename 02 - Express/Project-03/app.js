afe860b31e6719d707c092ea9f9f430bb45b5e53
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-27 11:04:08 +0600

    invalid route added and index.html used with express.static

--- 02 - Express/Project-03/app.js
+++ 02 - Express/Project-03/app.js
@@ -2,9 +2,23 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-// app.use(express.json());
+app.use(express.json());
 
+app.use(cors());
+
+app.use(express.static('public'))
+
+
+// home route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// invalid route
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Invalid route' });
+})
 
 module.exports = app;
--- 02 - Express/Project-03/index.js
+++ 02 - Express/Project-03/index.js
@@ -1,12 +1,6 @@
-const app = require('./app')
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to my server!!!</h1>');
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
