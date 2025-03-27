22bfa21b8eef07de07cd49a655cfd31b2dd1424b
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-28 00:09:38 +0600

    home route, error and invalid route created

--- 02 - Express/Project-04/index.js
+++ 02 - Express/Project-04/index.js
@@ -3,7 +3,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// home route
+app.get('/', (req, res) => {
+  res.send(`<h1>Welcome to My API</h1> <p>Version: ${process.env.API_VERSION}</p>`);
+});
 
+// invalid route
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Invalid route' });
+});
+// server error
+app.use((err, req, res, next) => {
+  res.status(404).json({ message: 'Something broke' });
+});
 
 
 app.listen(PORT, () => {
