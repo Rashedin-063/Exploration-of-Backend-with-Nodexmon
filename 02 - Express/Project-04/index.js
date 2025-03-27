29aef912cb61917995706ac6d6f43e47343b5712
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-28 00:08:16 +0600

    server created

--- 02 - Express/Project-04/index.js
+++ 02 - Express/Project-04/index.js
@@ -1,1 +1,11 @@
-console.log('hello')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
