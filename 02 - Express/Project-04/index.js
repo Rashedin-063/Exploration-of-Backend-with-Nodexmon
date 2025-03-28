3271ac3bcb468533770d654f1144017044005dc4
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-28 08:06:44 +0600

    .env file added

--- 02 - Express/Project-04/index.js
+++ 02 - Express/Project-04/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+console.log(PORT)
+
 
 // home route
 app.get('/', (req, res) => {
@@ -12,6 +14,7 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Invalid route' });
 });
+
 // server error
 app.use((err, req, res, next) => {
   res.status(404).json({ message: 'Something broke' });
