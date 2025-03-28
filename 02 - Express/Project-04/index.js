ecb354c064fe4bb80b380e38f7b298b5eefb4093
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-28 08:13:02 +0600

    app.js added

--- 02 - Express/Project-04/index.js
+++ 02 - Express/Project-04/index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
+const app = require('./app')
 const PORT = process.env.PORT || 3000;
-console.log(PORT)
 
 
 // home route
