f378ae8a762fa7e41dc78dffc845d0aba9609624
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-29 23:07:28 +0600

    server created for lesson 6

--- 02 - Express/Lesson-06/index.js
+++ 02 - Express/Lesson-06/index.js
@@ -1,1 +1,14 @@
-console.log('welcome to lesson 6')
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('', (req, res) => {
+  res.status(200).send('<h1>Welcome to my server</h1>');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+  
+})
