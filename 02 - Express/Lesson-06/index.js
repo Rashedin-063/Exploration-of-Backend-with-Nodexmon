39c148831a87b1d331883c089ad1cd8eb6ce478f
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-29 23:14:10 +0600

    route created for register

--- 02 - Express/Lesson-06/index.js
+++ 02 - Express/Lesson-06/index.js
@@ -4,8 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('', (req, res) => {
-  res.status(200).send('<h1>Welcome to my server</h1>');
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Welcome to my server from lesson 6</h1>');
+})
+app.get('/register', (req, res) => {
+  res.send('<h1>Welcome to my server from register</h1>');
+  res.status(200).sendFile(__dirname + 'index.html')
 })
 
 app.listen(PORT, () => {
