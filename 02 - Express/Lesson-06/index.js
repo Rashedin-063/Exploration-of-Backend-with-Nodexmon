1af325244eeb4f796802d86afd7c9b86d09d19a4
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-02 15:08:12 +0600

    updated

--- 02 - Express/Lesson-06/index.js
+++ 02 - Express/Lesson-06/index.js
@@ -5,8 +5,12 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('<h1>Welcome to my server from lesson 6</h1>');
-})
+  res.status(200).send(`
+    <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+      <h1>Welcome to my server from lesson 6</h1>
+    </div>
+  `);
+});
 app.get('/register', (req, res) => {
   // res.send('<h1>Welcome to my server from register</h1>');
   res.status(200).sendFile(__dirname + '/index.html')
