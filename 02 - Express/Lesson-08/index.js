347ebb29845278202157cb9fd8707adf2ff41d1e
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-03 14:51:06 +0600

    basic server created

--- 02 - Express/Lesson-08/index.js
+++ 02 - Express/Lesson-08/index.js
@@ -1,1 +1,17 @@
-console.log('lesson 8')
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+  res.status(200).send(`
+    <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+      <h1>Welcome to my server from lesson 8</h1>
+    </div>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`The Server is Running at ${PORT}`);
+  
+})
