880583e5e800bcbec587f1be52b55e9bd829e17e
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-04 16:39:03 +0600

    validation added for email

--- 02 - Express/Project-05/middlewares/userValidator.middleware.js
+++ 02 - Express/Project-05/middlewares/userValidator.middleware.js
@@ -6,7 +6,15 @@ const userValidator = [
     .notEmpty()
     .withMessage('Name is required')
     .isLength({ min: 4 })
-    .withMessage('Name should be at least 4 characters long'),
+    .withMessage('Name should be at least 4 characters long')
+    .isLength({ max: 30 })
+    .withMessage('Name can not exceed 30 characters'),
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Please enter a valid email address'),
 
   // Middleware to handle validation result
   (req, res, next) => {
