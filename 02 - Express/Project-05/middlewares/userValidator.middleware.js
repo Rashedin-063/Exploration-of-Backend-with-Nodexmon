7f60aa1e82e07a8356d51a8addc897adc55549fc
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-04 15:53:16 +0600

    middleware used

--- 02 - Express/Project-05/middlewares/userValidator.middleware.js
+++ 02 - Express/Project-05/middlewares/userValidator.middleware.js
@@ -1,5 +1,16 @@
 const { body } = require('express-validator');
 
-const userValidator = body('name').trim().notEmpty();
+const userValidator = [
+  body('name').trim().notEmpty().withMessage('Name is required'),
+
+  // Middleware to handle validation result
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
 
 module.exports = userValidator;
