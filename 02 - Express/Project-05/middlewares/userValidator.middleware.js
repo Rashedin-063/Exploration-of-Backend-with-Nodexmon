525f6939b3a73b4928d846414d3a46c2640ba885
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-04 19:15:29 +0600

    couldn't solve the validation skip problem

--- 02 - Express/Project-05/middlewares/userValidator.middleware.js
+++ 02 - Express/Project-05/middlewares/userValidator.middleware.js
@@ -1,21 +1,14 @@
-const { validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
+const userValidator =
 
-
-const userValidator = (req, res, next) => {
-  console.log('middleware.validator')
-  
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    let errorsList = errors.array().map((error) => error.msg);
-    // return res.status(422).json({ errors: errorsList });
-    return res.status(422).send({
-      success: false,
-      message: errors.array()[0].msg,
-    });
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
   }
-  next();
-};
 
 
 module.exports = userValidator;
