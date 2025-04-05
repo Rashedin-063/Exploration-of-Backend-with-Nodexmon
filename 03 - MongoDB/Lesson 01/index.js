252600adb768fe091c9087ea164a6f2c38add552
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-05 20:20:54 +0600

    basic home route created

--- 03 - MongoDB/Lesson 01/index.js
+++ 03 - MongoDB/Lesson 01/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-
+// basic home route
+app.get('/', (req, res) => {
+  res.status(200).send(`
+    <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+      <h1>Welcome to my server from project 5</h1>
+    </div>
+  `);
+});
 
 
 
