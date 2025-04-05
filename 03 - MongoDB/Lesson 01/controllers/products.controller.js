80222aa6f471ce3dea1bf37a2cc3908fb39c5eb8
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-05 22:34:57 +0600

    multiple data created in the db with insertMany

--- 03 - MongoDB/Lesson 01/controllers/products.controller.js
+++ 03 - MongoDB/Lesson 01/controllers/products.controller.js
@@ -15,14 +15,22 @@ const createProduct = async (req, res) => {
 
   try {
     // create a new product
-    const { title, price, quantity } = req.body;
+    // const { title, price, quantity } = req.body;
 
-    const newProduct = new Product({title, price, quantity});
+    // const newProduct = new Product({title, price, quantity});
 
     // save the product to the database
-    const savedProduct = await newProduct.save();
+    // const savedProduct = await newProduct.save()
 
-    res.status(201).send(savedProduct);
+    const savedMultipleProducts = await Product.insertMany([
+      { title: 'Samsung Tab', price: '40000', quantity: 1 },
+      { title: 'iPhone 12', price: '100000', quantity: 5 },
+      { title: 'Google Pixel 5', price: '70000', quantity: 3 },
+      { title: 'Xiaomi Mi 11', price: '60000', quantity: 2 },
+    ]);
+
+    // res.status(201).send(savedProduct);
+    res.status(201).send(savedMultipleProducts);
   } catch (error) {
     res.status(500).send({message: error.message});
   }
