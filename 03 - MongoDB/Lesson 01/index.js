714a2891b3e913f036c01ff8d2d200e765e90636
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-05 20:28:07 +0600

    mongodb connection established with mongoose

--- 03 - MongoDB/Lesson 01/index.js
+++ 03 - MongoDB/Lesson 01/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+
+const dbConnect = require('./utils/dbConnect');
 const PORT = 3000;
+
+
+dbConnect()
 
 // basic home route
 app.get('/', (req, res) => {
   res.status(200).send(`
     <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-      <h1>Welcome to my server from project 5</h1>
+      <h1>Welcome to my server from mongoDB lesson 1</h1>
     </div>
   `);
 });
---
+++ 03 - MongoDB/Lesson 01/utils/dbConnect.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    console.log('mongodb connection established');
+  } catch (error) {
+    console.error({ message: error.message });
+  }
+};
+
+module.exports = dbConnect;
