75bcaef1303ee80c8ad3fb5839e57ed8de1f08f7
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-05 21:23:08 +0600

    product schema created

---
+++ 03 - MongoDB/Lesson 01/model/product.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Product', productSchema)
