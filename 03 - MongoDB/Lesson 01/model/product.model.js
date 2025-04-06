f94c1098ec210c393f2f0dffa299fda4ce8a32a7
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-06 12:49:24 +0600

    mongoose built in validation explored

--- 03 - MongoDB/Lesson 01/model/product.model.js
+++ 03 - MongoDB/Lesson 01/model/product.model.js
@@ -3,23 +3,36 @@ const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: [true, 'product title is required'],
+    minLength: [3, 'minimum title length is three'],
+    lowercase: true,
+    // uppercase: true,
+    trim: true,
+    // enum: {
+    //   values: ['iphone', 'samsung', 'android', 'blackberry'],
+    //   message: '{VALUE} is not supported'
+    // },
   },
-  price: { 
+  price: {
     type: Number,
     required: true,
-    min: 0
+    min: [100, 'minimum price should be 100'],
+    max: [2000, 'maximum price should be within 2000'],
   },
   quantity: {
     type: Number,
     required: true,
-    min: 0
+    min: 0,
+  },
+ email:  {
+   type: String,
+   unique: true
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
 const Product = mongoose.model('Product', productSchema)
 
