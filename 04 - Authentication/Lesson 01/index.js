312ae99c5887db8c9eaeb1596a8fbf7b9e8185ea
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-10 14:16:36 +0600

    products route created

--- 04 - Authentication/Lesson 01/index.js
+++ 04 - Authentication/Lesson 01/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoute = require('./routes/auth.route');
-
+const productRoute = require('./routes/product.route');
 
 app.use(cors());
 app.use(express.json());
@@ -15,25 +15,26 @@ app.get('/', (req, res) => {
   res.status(200).sendFile(__dirname + '/views/index.html');
 });
 
-
 // auth route
 app.use('/api/register', authRoute);
 
+// product route
+app.use('/api/product', productRoute);
+
 // handling server error
-app.use((error, req, res, next) => { 
+app.use((error, req, res, next) => {
   res.status(500).json({
-    message: "Internal Server Error",
-  })
-})
+    message: 'Internal Server Error',
+  });
+});
 
 // route not found
-app.use((req, res, next) => { 
+app.use((req, res, next) => {
   res.status(404).json({
-    message: "Route not found",
-  })
-})
+    message: 'Route not found',
+  });
+});
 
-
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
