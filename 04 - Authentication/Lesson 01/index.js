b1c4de686084b1d56ef32886d7e2d65880862161
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-10 11:52:08 +0600

    basic server created

--- 04 - Authentication/Lesson 01/index.js
+++ 04 - Authentication/Lesson 01/index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(PORT, () => { 
+  console.log(`Server is running on port ${PORT}`);
+})
