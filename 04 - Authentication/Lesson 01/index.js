4de3042d795a5472213d01ae3a32ac213a4b4905
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-10 12:02:09 +0600

    error and not found route created

--- 04 - Authentication/Lesson 01/index.js
+++ 04 - Authentication/Lesson 01/index.js
@@ -13,7 +13,19 @@ app.get('/', (req, res) => {
   res.status(200).sendFile(__dirname + '/views/index.html');
 });
 
+// handling server error
+app.use((error, req, res, next) => { 
+  res.status(500).json({
+    message: "Internal Server Error",
+  })
+})
 
+// route not found
+app.use((req, res, next) => { 
+  res.status(404).json({
+    message: "Route not found",
+  })
+})
 
 
 app.listen(PORT, () => { 
