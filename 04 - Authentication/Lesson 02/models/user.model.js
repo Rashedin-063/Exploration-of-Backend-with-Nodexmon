81735b02ca96e652a4f6b4868e249b1c29ded6a4
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-11 13:07:32 +0600

    user password saved in the db using mongoose-encryption

--- 04 - Authentication/Lesson 02/models/user.model.js
+++ 04 - Authentication/Lesson 02/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const { validate } = require('./product.model');
 
 const userSchema = new mongoose.Schema({
@@ -23,6 +24,14 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+const encKey = process.env.ENC_KEY;
+
+// encrypt password regardless of any other options. name and _id will be left unencrypted
+userSchema.plugin(encrypt, {
+  secret: encKey,
+  encryptedFields: ['password'],
 });
 
 const User = mongoose.model('User', userSchema);
