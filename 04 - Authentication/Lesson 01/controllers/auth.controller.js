293f4cc3502b1edf546a199d8a3bb534ed8f1490
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-11 12:09:32 +0600

    users data retrived from db

--- 04 - Authentication/Lesson 01/controllers/auth.controller.js
+++ 04 - Authentication/Lesson 01/controllers/auth.controller.js
@@ -1,11 +1,27 @@
 const User = require("../models/user.model");
 
 
-const getRegisteredUsers = (req, res) => {
-  res.status(200).json({
-    message: 'Welcome to the registration API',
-  });
-};
+const getRegisteredUsers = async (req, res) => {
+  
+ try {
+   const users = await User.find({});
+   if(users.length === 0) {
+     return res.status(404).json({
+       message: 'No users found',
+     });
+   }
+   res.status(200).json({
+     message: 'Users found',
+     users: users,
+   });
+ } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+  
+ }
+
 
 const getLoginUsers = (req, res) => {
   res.status(200).json({
