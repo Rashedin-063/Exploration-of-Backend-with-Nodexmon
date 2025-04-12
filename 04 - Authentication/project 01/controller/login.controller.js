70cd9056ac34f2823a08772824ba9d391ef022c6
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-12 21:35:18 +0600

    login and register post route created

--- 04 - Authentication/project 01/controller/login.controller.js
+++ 04 - Authentication/project 01/controller/login.controller.js
@@ -5,7 +5,22 @@ const loginPage = (req, res) => {
 }
 
 const loginUser = (req, res) => {
-  res.render('login');
+ try {
+  
+   res.status(200).json({
+      message: 'Login successful',
+      status: true,
+      error: null
+   })
+   
+ } catch (error) {
+   res.status(500).json({
+      message: 'Server error',
+      status: false,
+      data: null,
+      error: error.message
+   })
+ }
 };
 
 module.exports = { loginUser, loginPage };
