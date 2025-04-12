a6e8d82443e01b93e8bf032b07d3e5a62cfb36ad
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-12 15:14:22 +0600

    basic server created

--- 04 - Authentication/project 01/index.js
+++ 04 - Authentication/project 01/index.js
@@ -1,1 +1,13 @@
-console.log('hello from authentication project 01');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
