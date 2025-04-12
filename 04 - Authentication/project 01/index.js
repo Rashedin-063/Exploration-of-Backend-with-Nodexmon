6b428302afc870ce65262e5ab59b6d8f737213bc
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-12 17:24:37 +0600

    index.ejs file rendered instead of html

--- 04 - Authentication/project 01/index.js
+++ 04 - Authentication/project 01/index.js
@@ -5,11 +5,14 @@ const cors = require('cors');
 const ejs = require('ejs');
 const PORT = process.env.PORT || 3000;
 
-
+app.set('view engine', 'ejs');
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  // res.sendFile(__dirname + '/views/index.html');
+  res.render('index');
 });
 
 app.listen(PORT, () => {
