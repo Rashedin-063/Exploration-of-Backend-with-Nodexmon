7301d2bbcf585436be39ab144e42f6ad985f3d94
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-13 17:18:45 +0600

    updated

--- 04 - Authentication/project 01/controller/register.controller.js
+++ 04 - Authentication/project 01/controller/register.controller.js
@@ -1,46 +1,26 @@
+const express = require('express');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const passport = require('passport');
+const session = require('express-session');
+
+const app = express();
+app.set('trust proxy', 1); // trust first proxy
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true },
+  })
+);
+
 const User = require('../models/user.model');
 
 const getRegisteredUser = async (req, res) => {
   res.render('register');
 };
 
-// Register Controller
-// const registerUser = async (req, res) => {
-//   try {
-//     const { username, email, password } = req.body;
-
-//     // Check if all fields are provided
-//     if (!username || !email || !password) {
-//       return res.status(400).json({ message: 'All fields are required' });
-//     }
-
-//     // Check if the user already exists
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json({ message: 'User already exists' });
-//     }
-
-//     // Hash the password
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     // Create a new user
-//     const newUser = new User({
-//       username,
-//       email,
-//       password: hashedPassword,
-//     });
-
-//     // Save the user to the database
-//     await newUser.save();
-
-//     res.status(201).json({ message: 'User registered successfully' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Server error' });
-//   }
-// };
 
 const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
