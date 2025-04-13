6738bf8c8d65d5d85d4753292f8f89915646038f
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-13 14:17:39 +0600

    password hassed to increase security

--- 04 - Authentication/project 01/controller/register.controller.js
+++ 04 - Authentication/project 01/controller/register.controller.js
@@ -1,9 +1,10 @@
-const User = require("../models/user.model");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const User = require('../models/user.model');
 
-
-const getRegisteredUser = async (req, res) => { 
-  res.render('register')
-}
+const getRegisteredUser = async (req, res) => {
+  res.render('register');
+};
 
 // Register Controller
 // const registerUser = async (req, res) => {
@@ -41,31 +42,41 @@ const getRegisteredUser = async (req, res) => {
 //   }
 // };
 
-const registerUser = async (req, res) => { 
+const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
-  
+
   try {
+    const user = await User.findOne({ email: email });
+
+    if (user) return res.status(400).json({ message: 'User already exists' });
+
+    bcrypt.hash(password, saltRounds, async function (err, hash) {
+        if (err) {
+          console.error('Error hashing password:', err);
+          return res
+            .status(500)
+            .json({ message: 'Error while hashing password' });
+        }
+      const newUser = new User({
+      userName,
+      email,
+      password: hash,
+    });
+
+    await newUser.save();
+
+    res.status(201).redirect('/login');
+    });
+
    
-    const user = await User.findOne({ email: email })
-    
-    if (user) return res.status(400).json({ message: 'User already exists' })
-    
-    const newUser = new User({
-      userName, email, password
-    })
-
-    await newUser.save()
-
-   res.status(201).redirect('/login');
- } catch (error) {
-   console.error(error);
+  } catch (error) {
+    console.error(error);
     res.status(500).json({
       success: false,
       message: error.message ? error.message : 'Server error',
       data: null,
     });
-  
- }
-}
+  }
+};
 
 module.exports = { registerUser, getRegisteredUser };
