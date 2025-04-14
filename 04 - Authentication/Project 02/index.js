9563600b6c2cd32b8e47a2c9480325ea97f18a73
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-14 23:24:02 +0600

    error solved

--- 04 - Authentication/Project 02/index.js
+++ 04 - Authentication/Project 02/index.js
@@ -12,6 +12,7 @@ const logoutRoute = require('./routes/logout.route');
 
 
 // Middleware
+app.set('view engine', 'ejs');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
