e6333e33c9bbd43fed010c899fa9c1f46054204b
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-14 23:22:38 +0600

    updated

--- 04 - Authentication/Project 02/index.js
+++ 04 - Authentication/Project 02/index.js
@@ -5,14 +5,27 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const registerRoute = require('./routes/register.route');
+const loginRoute = require('./routes/login.route');
+const profileRoute = require('./routes/profile.route');
+const logoutRoute = require('./routes/logout.route');
+
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome to the Express server!');
+  res.render('index');
 });
+
+// routes
+app.use('/register', registerRoute);
+app.use('/login', loginRoute);
+app.use('/profile', profileRoute);
+app.use('/logout', logoutRoute);
 
 // Start the server
 app.listen(PORT, () => {
