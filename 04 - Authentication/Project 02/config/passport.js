61072b2e053f152382cb6f22dd1e8b44384224cc
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-15 12:52:10 +0600

    passport.js setup done

--- 04 - Authentication/Project 02/config/passport.js
+++ 04 - Authentication/Project 02/config/passport.js
@@ -1,55 +1,45 @@
-const passport = require("passport");
-const User = require("../models/user.model");
+const passport = require('passport');
+const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 
-
 passport.use(
-  new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
-    
-    try {
-      const user = await User.findOne({ email: email });
-      if (!user) {
-        return done(null, false, { message: 'Incorrect email' });
+  new LocalStrategy(
+    { usernameField: 'email' },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          return done(null, false, { message: 'Incorrect email' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+
+        // If the password is correct, return the user object
+        return done(null, user);
+      } catch (error) {
+        return done(error);
       }
-
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-
-      // If the password is correct, return the user object
-      return done(null, user);
-
-    } catch (error) {
-      return done(error);
-      
     }
-  })
+  )
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
-/**
- * passport.use(
-   new LocalStrategy(
-     { usernameField: 'email' },
-     async (email, password, done) => {
-       try {
-         const user = await User.findOne({ email: email });
-         if (!user) {
-           return done(null, false, { message: 'Incorrect email' });
-         }
- 
-         const isMatch = await bcrypt.compare(password, user.password);
-         if (!isMatch) {
-           return done(null, false, { message: 'Incorrect password' });
-         }
- 
- 
-         return done(null, user);
-       } catch (error) {
-         return done(error);
-       }
-     }
-   )
- );
- */
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, false);
+  }
+
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
