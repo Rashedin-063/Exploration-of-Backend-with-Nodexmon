f5fa618d62153258b79d4605cda80cf155661114
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-15 13:55:34 +0600

    logout functionality added

--- 04 - Authentication/Project 02/controller/logout.controller.js
+++ 04 - Authentication/Project 02/controller/logout.controller.js
@@ -1,22 +1,12 @@
 
 
 const logoutUser = (req, res) => {
-
-  try {
-     res.redirect('/');
-  //  req.logout((err) => {
-  //    if (err) {
-  //     return next(err)
-  //    }
-  //    res.redirect('/')
-  // })
- } catch (error) {
-  res.status(500).json({
-    success: false,
-    message: error.message ? error.message : 'Server error',
-    data: null,
-  });
- }
+   req.logout((err) => {
+     if (err) {
+      return next(err)
+     }
+     res.redirect('/')
+  })
 
 };
 
