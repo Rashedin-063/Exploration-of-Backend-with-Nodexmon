caf421c354376319f9e172a4bf9646e4f4d84576
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-16 11:43:22 +0600

    passport.js updated for google auth

--- 04 - Authentication/Project 03/config/passport.js
+++ 04 - Authentication/Project 03/config/passport.js
@@ -1,28 +1,20 @@
+require('dotenv').config();
 const passport = require('passport');
 const User = require('../models/user.model');
-const bcrypt = require('bcrypt');
-const LocalStrategy = require('passport-local').Strategy
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(
-  new LocalStrategy(
-    { usernameField: 'email' },
-    async (email, password, done) => {
-      try {
-        const user = await User.findOne({ email: email });
-        if (!user) {
-          return done(null, false, { message: 'Incorrect email' });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-          return done(null, false, { message: 'Incorrect password' });
-        }
-
-        // If the password is correct, return the user object
-        return done(null, user);
-      } catch (error) {
-        return done(error);
-      }
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://www.example.com/auth/google/callback',
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
     }
   )
 );
