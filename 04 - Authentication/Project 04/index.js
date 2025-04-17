a8b7b1d147fee6b344f603c8bfd4681feb397865
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-17 22:55:04 +0600

    public/style.css file added

--- 04 - Authentication/Project 04/index.js
+++ 04 - Authentication/Project 04/index.js
@@ -1,21 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Home route
 app.get('/', (req, res) => {
   res.send(`
     <html>
       <head>
-        <style>
-          body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-          }
-        </style>
+        <link rel="stylesheet" href="/style.css">
       </head>
       <body>
         <h1>Welcome to the Home Page</h1>
