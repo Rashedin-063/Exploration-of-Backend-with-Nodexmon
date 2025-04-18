6acc3b147cbcad50036a809a06856c32da8ea9b9
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-18 22:37:44 +0600

    user saved successfully

--- 04 - Authentication/Project 04/Server/controllers/register.controller.js
+++ 04 - Authentication/Project 04/Server/controllers/register.controller.js
@@ -1,17 +1,36 @@
-
+const bcrypt = require('bcrypt');
+const User = require('../models/user.model');
+const saltRounds = 10;
 
 const getRegisterPage = (req, res) => {
   res.status(200).json({ message: 'Register page' });
 };
 
-const registerUser = async(req, res) => {
+const registerUser = async (req, res) => {
   const { username, password } = req.body;
-  // Here you would normally save the user to the database
-  res.status(201).json({ message: 'User registered successfully' });
-};
 
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.status(400).json({ message: 'Username already exists' });
+  }
+
+  const newUser = new User({
+    username,
+    password: await bcrypt.hash(password, saltRounds),
+  });
+
+  await newUser.save();
+
+  res
+    .status(201)
+    .json({
+      success: true,
+      message: 'User registered successfully',
+      data: newUser,
+    });
+};
 
 module.exports = {
   getRegisterPage,
-  registerUser
+  registerUser,
 };
