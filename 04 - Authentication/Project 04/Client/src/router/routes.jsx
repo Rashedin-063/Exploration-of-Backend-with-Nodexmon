7eae9c854f8bae03c43cb63c1015fef79956b902
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-19 13:33:21 +0600

    routes updated

--- 04 - Authentication/Project 04/Client/src/router/routes.jsx
+++ 04 - Authentication/Project 04/Client/src/router/routes.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import Home from '../pages/Home';
+import Profile from './../pages/Profile';
+import Register from './../pages/Register';
+import Login from './../pages/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-     
+      { index: true, element: <Home /> },
+      { path: '/profile', element: <Profile /> },
+      { path: '/register', element: <Register /> },
+      {path: '/login', element: <Login />}, 
     ]
   },
 ]);
