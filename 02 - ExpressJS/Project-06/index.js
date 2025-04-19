eeae92c7fb9ef0b242acfcffc16eea048b8af0ac
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-19 21:58:45 +0600

    ui updated

--- 02 - ExpressJS/Project-06/index.js
+++ 02 - ExpressJS/Project-06/index.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path')
+const authRouter = require('./controller/auth.controller')
 
 const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+app.use(authRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to Express Project 6');
