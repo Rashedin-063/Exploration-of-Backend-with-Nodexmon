71246ff1761f8d0670af6193277d570aa61306a0
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-19 21:32:57 +0600

    basic server created

--- 02 - ExpressJS/Project-06/index.js
+++ 02 - ExpressJS/Project-06/index.js
@@ -1,2 +1,13 @@
-console.log('hello from express project 6')
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Express Project 6');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
