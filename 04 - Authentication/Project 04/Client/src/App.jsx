cd4833d669f2287ddbe30ef9129c6c8fdbf54270
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-19 12:35:12 +0600

    pages created

--- 04 - Authentication/Project 04/Client/src/App.jsx
+++ 04 - Authentication/Project 04/Client/src/App.jsx
@@ -1,6 +1,43 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import './index.css';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <Router>
+      <div className='layout'>
+        <nav className='navbar'>
+          <NavLink to='/' className='nav-link'>
+            Home
+          </NavLink>
+          <NavLink to='/profile' className='nav-link'>
+            Profile
+          </NavLink>
+          <NavLink to='/register' className='nav-link'>
+            Register
+          </NavLink>
+          <NavLink to='/login' className='nav-link'>
+            Login
+          </NavLink>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <footer className='footer'>&copy; 2025 Your Website</footer>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
