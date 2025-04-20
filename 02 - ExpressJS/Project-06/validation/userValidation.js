394ce2b2e1fe239a9e014ec0b2f397a8ca0546de
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-20 13:11:00 +0600

    login validation code separated

--- 02 - ExpressJS/Project-06/validation/userValidation.js
+++ 02 - ExpressJS/Project-06/validation/userValidation.js
@@ -1,5 +1,28 @@
 const Joi = require('joi');
 
+const loginValidation = (req, res, next) => {
+  // create a schema
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(8).required(),
+  });
+
+  // validate the data using schema
+  const { error } = schema.validate(req.body, {
+    abortEarly: false,
+    errors: { wrap: { label: '' } },
+  });
+
+  if (error) {
+    const errorList = error.details.map((err) => err.message);
+    res.status(400).json({
+      message: 'invalid input',
+      error: errorList,
+    });
+  }
+
+  next()
+}
 const registerValidation = (req, res, next) => {
   // create a schema
   // create a schema
@@ -27,4 +50,4 @@ const registerValidation = (req, res, next) => {
 }
 
 
-module.exports = registerValidation
+module.exports = {registerValidation, loginValidation}
